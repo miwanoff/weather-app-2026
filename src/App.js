5b1d6ce20b8ccfe417ae13aed106268ffdecb80b
miwanoff <marina.iva.noffa@gmail.com>
2026-04-26 13:05:49 +0300

    Add search

--- src/App.js
+++ src/App.js
@@ -27,6 +27,18 @@ function App() {
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
   return (
     <div className="container">
       <Header className="jumbotron alert alert-warning" />
@@ -41,9 +53,40 @@ function App() {
             type="text"
             className="form-control search-input"
             placeholder="Search"
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyDown={search}
           />
         </div>
       </div>
+      {typeof weather.main != "undefined" ? (
+        <div className="row">
+          <div className="search-panel col-md-4 col-sm-12 my-3">
+            <div className="card">
+              <div className="card-header bg-primary text-white">
+                <h2>
+                  {console.log(weather)}
+                  {weather.name}, {weather.sys.country}
+                </h2>
+              </div>
+              <div className="card-body bg-warning">
+                <div className="temp">
+                  <h3>{Math.round(weather.main.temp)}°c</h3>
+                </div>
+                <div className="weather">{weather.weather[0].main}</div>
+                <p className="card-text">{weather.weather[0].description}</p>
+                <div id="icon">
+                  <img
+                    src={`${api.icon}/${weather.weather[0].icon}.png`}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
